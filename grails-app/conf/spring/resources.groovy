c744244862ea042856bdf579f39abe1a24d4ba99
salo12 <31453092+salo12@users.noreply.github.com>
2019-01-04 16:01:26 -0300

    resource de products

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -8,6 +8,8 @@ import com.sma.delivery.rest.comments.CommentsResourceImpl
 import com.sma.delivery.service.comments.CommentsServiceImpl
 import com.sma.delivery.rest.establishments.EstablishmentsResourceImpl
 import com.sma.delivery.service.establishments.EstablishmentsServiceImpl
+import com.sma.delivery.rest.products.ProductsResourceImpl
+import com.sma.delivery.service.products.ProductsServiceImpl
 import login.MyAuthenticationProvider
 import org.springframework.web.servlet.i18n.SessionLocaleResolver
 
@@ -24,6 +26,8 @@ beans = {
     commentsService(CommentsServiceImpl)
     establishmentsResource(EstablishmentsResourceImpl)
     establishmentsService(EstablishmentsServiceImpl)
+    productsResource(ProductsResourceImpl)
+    productsService(ProductsServiceImpl)
 
     localeResolver(SessionLocaleResolver) {
         defaultLocale= new java.util.Locale('es');
