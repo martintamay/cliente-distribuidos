3c95d0560cdf10ad3ea3da4526ad707bd5229022
fabrizio123450 <fabrizio123450@gmail.com>
2019-02-14 01:31:24 -0300

    Agregado Cache a user en cliente

--- src/main/java/com/sma/delivery/service/base/BaseServiceImpl.java
+++ src/main/java/com/sma/delivery/service/base/BaseServiceImpl.java
@@ -2,6 +2,8 @@ package com.sma.delivery.service.base;
 
 import com.sma.delivery.beans.base.BaseBean;
 import com.sma.delivery.dto.base.BaseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 
 import java.text.ParseException;
 
@@ -9,6 +11,12 @@ public abstract class BaseServiceImpl<BEAN extends BaseBean, DTO extends BaseDTO
 
 	public BaseServiceImpl() {
 
+	}
+	@Autowired
+	private CacheManager cacheManager;
+
+	protected CacheManager getCacheManager() {
+		return cacheManager;
 	}
 
 	protected abstract BEAN convertDtoToBean(DTO dto) throws ParseException;
